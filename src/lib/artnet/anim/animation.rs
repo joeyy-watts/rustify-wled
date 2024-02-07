b2d27b33fb834082bab9711c63b4820dfa913c51
joeyy-watts <joeyms40@gmail.com>
2024-02-08 00:22:24 +0700

    update animation.rs

--- src/lib/artnet/anim/animation.rs
+++ src/lib/artnet/anim/animation.rs
@@ -3,14 +3,15 @@ use super::frame::AnimationFrame;
 
 #[derive(Clone)]
 pub struct Animation {
-    pub frames: Vec<AnimationFrame>
+    pub frames: Vec<AnimationFrame>,
+    pub target_fps: u8,
 }
 
 impl Animation {
     pub fn new(image: Vec<u8>, size: (u8, u8), target_fps: u8, effect: &dyn Effect) -> Self {
         let (width, height) = (32, 32);
-        let frames = effect.apply(&image);
+        let frames = effect.apply(&image, &target_fps);
 
-        Self { frames }
+        Self { frames, target_fps }
     }
 }
