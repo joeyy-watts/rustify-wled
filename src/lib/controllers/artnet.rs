e605f91f04891fbf8076b2ee61a0fe6b88948373
joeyy-watts <joeyms40@gmail.com>
2024-02-09 00:07:34 +0700

    refactor ArtNetController; implement stop flag

--- src/lib/controllers/artnet.rs
+++ src/lib/controllers/artnet.rs
@@ -1,5 +1,8 @@
 use std::net::{UdpSocket, ToSocketAddrs};
-use std::cmp;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use std::{cmp, thread};
 use artnet_protocol::*;
 use crate::lib::artnet::anim::frame::AnimationFrame;
 
@@ -13,22 +16,59 @@ use crate::lib::artnet::anim::frame::AnimationFrame;
 /// `dimensions` - height and weight of the target device
 /// 
 pub struct ArtNetController2D {
+    pub inner: Arc<ArtNetController2DInner>,
+    stop_flag: Arc<AtomicBool>
+} 
+
+pub struct ArtNetController2DInner {
     pub target: String,
     pub dimensions: (u16, u16),
     socket: UdpSocket,
+    sequence_counter: Mutex<u8>,
 }
 
 impl ArtNetController2D {
     pub fn new(target: String, dimensions: (u16, u16)) -> Self {
+        let inner = Arc::new(ArtNetController2DInner::new(target, dimensions));
+        let stop_flag = Arc::new(AtomicBool::new(false));
+
+        Self { inner, stop_flag }
+    }
+
+    pub fn send_frames(&self, frames: Vec<AnimationFrame>, frame_interval: f64) {
+        let local_self = self.inner.clone();
+        let local_flag = self.stop_flag.clone();
+
+        thread::spawn(move || {
+            while !local_flag.load(Ordering::Relaxed) {
+                for frame in frames.clone() {
+                    local_self.send_frame(frame);
+                    thread::sleep(Duration::from_secs_f64(frame_interval));
+                }
+            }
+        });
+    }
+
+    pub fn stop_animation(&self) {
+        self.stop_flag.store(true, Ordering::Relaxed);
+    }
+}
+
+///
+/// Inner struct for ArtNetController2D
+/// to be used inside a thread
+impl ArtNetController2DInner {
+    pub fn new(target: String, dimensions: (u16, u16)) -> Self {
         let socket = UdpSocket::bind("0.0.0.0:0").expect("Unable to bind to address!");
-        Self { target, dimensions, socket }
+        let sequence_counter: Mutex<u8> = Mutex::new(0);
+        Self { target, dimensions, socket, sequence_counter } //, stop_flag }
     }
 
     /// Sends a single frame (or image) to the target device
     /// 
     /// `frame` - the frame to be sent
     /// 
-    pub fn send_frame(&self, frame: AnimationFrame) {
+    fn send_frame(&self, frame: AnimationFrame) {
         // or channels per universe
         static CHANNELS_PER_SHARD: u16 = 510;
         static CHANNELS_PER_PIXEL: u16 = 3;
@@ -37,7 +77,9 @@ impl ArtNetController2D {
         // total num of channels in frame = (width * height) * (channels per pixel, 3)
         // we can fit only 170 pixels/510 channels in a single universe, even though the max is 512
         // num of shards = ceil(total num of channels / 510)
-        let num_shards: u16 = ((((self.dimensions.0 * self.dimensions.1 * CHANNELS_PER_PIXEL) as u32) / CHANNELS_PER_SHARD as u32) + 1) as u16;
+        let num_shards: u16 = (
+            (((self.dimensions.0 * self.dimensions.1 * CHANNELS_PER_PIXEL) as u32) / CHANNELS_PER_SHARD as u32) + 1
+        ) as u16;
 
         let addr = format!("{}:6454", self.target).to_socket_addrs().unwrap().next().unwrap();
 
@@ -49,11 +91,16 @@ impl ArtNetController2D {
 
             let command: ArtCommand = ArtCommand::Output(Output {
                 data: frame_slice.into(), // The data we're sending to the node
-                port_address: PortAddress::try_from(u as u8).unwrap(),
+                sequence: self.sequence_counter.lock().unwrap().clone(),
+                port_address: PortAddress::try_from((u + 1) as u8).unwrap(),
                 ..Output::default()
             });
+            
             let bytes = command.write_to_buffer().unwrap();
             self.socket.send_to(&bytes, &addr).unwrap();
+
+            let mut sequence_counter = self.sequence_counter.lock().unwrap();
+            *sequence_counter = sequence_counter.wrapping_add(1);
         }
     }
 }
