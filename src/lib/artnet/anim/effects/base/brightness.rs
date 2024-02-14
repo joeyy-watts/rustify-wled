766a9f20ef5fc630d9d9f19ece2cd6d85ba0fabe
joeyy-watts <joeyms40@gmail.com>
2024-02-15 00:05:05 +0700

    update brightness.rs to support EffectBuilder

--- src/lib/artnet/anim/effects/base/brightness.rs
+++ src/lib/artnet/anim/effects/base/brightness.rs
@@ -1,25 +1,19 @@
 use crate::lib::artnet::anim::frame::AnimationFrame;
-use crate::lib::artnet::anim::effects::effect::Effect;
+use crate::lib::artnet::anim::effects::effect::{WaveformEffect, WaveformParameters};
 use self::super::math::Math;
 
-pub struct SinBrightnessEffect {
-    pub period: f64,
-    pub amplitude: f64,
-    pub offset: f64,
 
+struct SinEffect;
+struct TruncSinEffect;
+
+impl WaveformEffect for SinEffect {
+    fn math_func(&self, waveform_params: WaveformParameters) -> f64 {
+        Math::sin_wave(waveform_params.amplitude, waveform_params.period, waveform_params.offset, waveform_params.exponent)
+    }
 }
 
-impl Effect for SinBrightnessEffect {
-    fn apply(&self, image: &Vec<u8>, target_fps: &u8) -> Vec<AnimationFrame> {
-        let mut result = vec![];
-        let num_factors = (((f64::from(*target_fps) * self.period).round())) as u16;
-
-        // calculate multipliers from sine wave
-        for i in 0..num_factors {
-            let multiplier = Math::sin_wave((i as f64 / num_factors as f64), self.amplitude, self.period, self.offset, 1.0);
-            result.push(AnimationFrame::new(&(image.iter().map(|x| (*x as f64 * multiplier) as u8).collect())));
-        }
-
-        result
+impl WaveformEffect for TruncSinEffect {
+    fn math_func(&self, waveform_params: WaveformParameters) -> f64 {
+        Math::trunc_sin_wave(waveform_params.amplitude, waveform_params.period, waveform_params.offset, waveform_params.exponent)
     }
 }
