e80e8f873226e8a1339051290c8238acd9d306a7
joeyy-watts <joeyms40@gmail.com>
2024-02-15 00:50:04 +0700

    make effect structs pub, use correct type

--- src/lib/artnet/anim/effects/base/brightness.rs
+++ src/lib/artnet/anim/effects/base/brightness.rs
@@ -2,17 +2,17 @@ use crate::lib::artnet::anim::effects::effect::{WaveformEffect, WaveformParamete
 use self::super::math::Math;
 
 
-struct SinEffect;
-struct TruncSinEffect;
+pub struct SinEffect;
+pub struct TruncSinEffect;
 
 impl WaveformEffect for SinEffect {
-    fn math_func(&self, i: u16, waveform_params: WaveformParameters) -> f64 {
+    fn math_func(&self, i: f64, waveform_params: WaveformParameters) -> f64 {
         Math::sin_wave(i as f64, waveform_params.amplitude, waveform_params.period, waveform_params.offset, waveform_params.exponent)
     }
 }
 
 impl WaveformEffect for TruncSinEffect {
-    fn math_func(&self, i: u16, waveform_params: WaveformParameters) -> f64 {
+    fn math_func(&self, i: f64, waveform_params: WaveformParameters) -> f64 {
         Math::trunc_sin_wave(i as f64, waveform_params.amplitude, waveform_params.period, waveform_params.offset, waveform_params.exponent)
     }
 }
