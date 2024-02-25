49493104481fe039aa68f97c565e8855395460de
Joey <joeyms40@gmail.com>
2024-02-25 16:48:30 +0700

    add transitions to Animation class

--- src/lib/artnet/anim/animation.rs
+++ src/lib/artnet/anim/animation.rs
@@ -3,14 +3,25 @@ use super::frame::AnimationFrame;
 
 #[derive(Clone)]
 pub struct Animation {
-    pub frames: Vec<AnimationFrame>,
     pub target_fps: u8,
+    pub frames_loop: Vec<AnimationFrame>,
+    // optional in/out transition frames
+    pub frames_in: Option<Vec<AnimationFrame>>,
+    pub frames_out: Option<Vec<AnimationFrame>>,
+    image: Vec<u8>,
 }
 
 impl Animation {
     pub fn new(image: Vec<u8>, target_fps: u8, effect: RenderedEffect) -> Self {
-        let frames = effect.apply(&image);
+        let frames_loop = effect.apply(&image);
+        Self { target_fps, frames_loop, frames_in: None, frames_out: None, image }
+    }
 
-        Self { frames, target_fps }
+    pub fn add_transition_in(&mut self, effect: RenderedEffect) {
+        self.frames_in = Some(effect.apply(&self.image));
+    }
+
+    pub fn add_transition_out(&mut self, effect: RenderedEffect) {
+        self.frames_out = Some(effect.apply(&self.image));
     }
 }
