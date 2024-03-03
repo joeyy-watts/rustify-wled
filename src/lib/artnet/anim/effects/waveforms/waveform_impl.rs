1a958db3914a2ecbb30f8e4fe30a97d45b653539
Joey <joeyms40@gmail.com>
2024-03-03 23:00:16 +0700

    use v_offset and h_offset in waveform_impl

--- src/lib/artnet/anim/effects/waveforms/waveform_impl.rs
+++ src/lib/artnet/anim/effects/waveforms/waveform_impl.rs
@@ -7,18 +7,18 @@ pub struct SawtoothEffect;
 
 impl WaveformEffect for SinEffect {
     fn math_func(&self, i: f64, waveform_params: WaveformParameters) -> f64 {
-        Math::sin_wave(i as f64, waveform_params.amplitude, waveform_params.period, waveform_params.offset, waveform_params.exponent)
+        Math::sin_wave(i as f64, waveform_params.amplitude, waveform_params.period, waveform_params.v_offset, waveform_params.h_offset, waveform_params.exponent)
     }
 }
 
 impl WaveformEffect for TruncSinEffect {
     fn math_func(&self, i: f64, waveform_params: WaveformParameters) -> f64 {
-        Math::trunc_sin_wave(i as f64, waveform_params.amplitude, waveform_params.period, waveform_params.offset, waveform_params.exponent)
+        Math::trunc_sin_wave(i as f64, waveform_params.amplitude, waveform_params.period, waveform_params.v_offset, waveform_params.h_offset, waveform_params.exponent)
     }
 }
 
 impl WaveformEffect for SawtoothEffect {
     fn math_func(&self, i: f64, waveform_params: WaveformParameters) -> f64 {
-        Math::sawtooth(i as f64, waveform_params.amplitude, waveform_params.period, waveform_params.offset)
+        Math::sawtooth(i as f64, waveform_params.amplitude, waveform_params.period, waveform_params.v_offset, waveform_params.h_offset)
     }
 }
