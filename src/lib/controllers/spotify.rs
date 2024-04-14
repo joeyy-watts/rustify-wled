bdf1066466febb8a32984776c0d0d1c24e467677
Joey <joeyms40@gmail.com>
2024-04-14 15:58:21 +0700

    refactor spotify.rs

--- src/lib/controllers/spotify.rs
+++ src/lib/controllers/spotify.rs
@@ -71,75 +71,17 @@ impl PlaybackState {
 }
 
 impl SpotifyController {
-    pub fn new(sender: Sender<PlaybackState>) -> Self {
-        let config = Config {
-            token_cached: true,
-            ..Default::default()
-        };
-
-        let credentials: Credentials = match Credentials::from_env() {
-            Some(_) => {
-                Credentials::from_env().unwrap()
-            },
-            None => {
-                SpotifyController::id_secret_prompt().unwrap()
-            }
-        };
-
-        let oauth: OAuth = OAuth {
-            redirect_uri: "http://localhost:8000/callback".to_string(),
-            scopes: scopes!(
-                "user-read-playback-state",
-                "user-read-currently-playing"
-            ),
-            ..Default::default()
-          
-        };
-
-        let client = Arc::new(AuthCodeSpotify::with_config(credentials, oauth, config));
+    pub fn new(anim_sender: Sender<PlaybackState>) -> Self {
+        let client = Arc::new(SpotifyController::get_client());
 
         let stop_flag: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
 
-        let sender: Arc<Sender<PlaybackState>> = Arc::new(sender);
+        // sender to send message to AnimationController
+        let sender: Arc<Sender<PlaybackState>> = Arc::new(anim_sender);
 
         let current_playing = Arc::new(PlaybackState::none());
    
         Self { client, stop_flag, sender, current_playing }
-    }
-
-    fn id_secret_prompt() -> Option<Credentials> {
-        println!("RSPOTIFY_CLIENT_ID/RSPOTIFY_CLIENT_SECRET not found in environment");
-        print!("Enter RSPOTIFY_CLIENT_ID: ");
-        let _ = io::stdout().flush();
-        let mut client_id = String::new();
-        io::stdin().read_line(&mut client_id).expect("Unable to read RSPOTIFY_CLIENT_ID");
-
-        print!("Enter RSPOTIFY_CLIENT_SECRET: ");
-        let mut client_secret = String::new();
-        let _ = io::stdout().flush();
-        io::stdin().read_line(&mut client_secret).expect("Unable to read RSPOTIFY_CLIENT_SECRET");
-
-        if client_id.len() == 0 || client_secret.len() == 0 {
-            panic!("RSPOTIFY_CLIENT_ID and RSPOTIFY_CLIENT_SECRET cannot be empty!")
-        }
-
-        // persist client id/secret to env var
-        env::set_var("RSPOTIFY_CLIENT_ID", client_id.clone());
-        env::set_var("RSPOTIFY_CLIENT_SECRET", client_secret.clone());
-
-        Some(Credentials::new(client_id.trim().as_ref(), client_secret.trim().as_ref()))
-    }
-
-    pub fn get_token(&self) -> Option<Token> {
-         self.client.get_token().lock().unwrap().clone()
-    }
-
-    pub fn get_authorize_url(&self) -> String {
-        self.client.get_authorize_url(false).unwrap()
-    }
-
-    pub fn get_access_token(&self, code: &str) -> Result<(), ClientError> {
-        self.client.request_token(code)
     }
 
     pub fn start_listening(&self) {
@@ -203,5 +145,72 @@ impl SpotifyController {
 
     pub fn stop_listening(&self) {
         self.stop_flag.store(true, Ordering::Relaxed);
+    }
+
+    /////////////////////////////////////////
+    // rspotify client related methods
+    ////////////////////////////////////////
+    
+    fn get_client() -> AuthCodeSpotify {
+        let config = Config {
+            token_cached: true,
+            ..Default::default()
+        };
+
+        let credentials: Credentials = match Credentials::from_env() {
+            Some(_) => {
+                Credentials::from_env().unwrap()
+            },
+            None => {
+                SpotifyController::id_secret_prompt().unwrap()
+            }
+        };
+
+        let oauth: OAuth = OAuth {
+            redirect_uri: "http://localhost:8000/callback".to_string(),
+            scopes: scopes!(
+                "user-read-playback-state",
+                "user-read-currently-playing"
+            ),
+            ..Default::default()
+          
+        };
+
+        AuthCodeSpotify::with_config(credentials, oauth, config)
+    }
+
+    fn id_secret_prompt() -> Option<Credentials> {
+        println!("RSPOTIFY_CLIENT_ID/RSPOTIFY_CLIENT_SECRET not found in environment");
+        print!("Enter RSPOTIFY_CLIENT_ID: ");
+        let _ = io::stdout().flush();
+        let mut client_id = String::new();
+        io::stdin().read_line(&mut client_id).expect("Unable to read RSPOTIFY_CLIENT_ID");
+
+        print!("Enter RSPOTIFY_CLIENT_SECRET: ");
+        let mut client_secret = String::new();
+        let _ = io::stdout().flush();
+        io::stdin().read_line(&mut client_secret).expect("Unable to read RSPOTIFY_CLIENT_SECRET");
+
+        if client_id.len() == 0 || client_secret.len() == 0 {
+            panic!("RSPOTIFY_CLIENT_ID and RSPOTIFY_CLIENT_SECRET cannot be empty!")
+        }
+
+        // persist client id/secret to env var
+        env::set_var("RSPOTIFY_CLIENT_ID", client_id.clone());
+        env::set_var("RSPOTIFY_CLIENT_SECRET", client_secret.clone());
+
+        Some(Credentials::new(client_id.trim().as_ref(), client_secret.trim().as_ref()))
+    }
+
+    pub fn get_token(&self) -> Option<Token> {
+         self.client.get_token().lock().unwrap().clone()
+    }
+
+    pub fn get_authorize_url(&self) -> String {
+        self.client.get_authorize_url(false).unwrap()
+    }
+
+    pub fn get_access_token(&self, code: &str) -> Result<(), ClientError> {
+        self.client.request_token(code)
     }
 }
